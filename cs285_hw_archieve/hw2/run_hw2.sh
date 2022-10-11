2e9c9da7c5c08a14dfef95125093f913965a4197
wenqian <wenqian_xu@hotmail.com>
2022-10-11 21:41:16 +0800

    hw1-hw4 code completed

---
+++ cs285_hw_archieve/hw2/run_hw2.sh
@@ -0,0 +1,33 @@
+# q1
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 1000 -dsa --exp_name q1_sb_no_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 1000 -rtg -dsa --exp_name q1_sb_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 1000 -rtg --exp_name q1_sb_rtg_na
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 -dsa --exp_name q1_lb_no_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 -rtg -dsa --exp_name q1_lb_rtg_dsa
+python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 -rtg --exp_name q1_lb_rtg_na
+
+# q2
+for b in 1000, 1500, 2000, 2500, 3000; do 
+    python run_hw2.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.9 -n 100 -l 2 -s 64 -b $b -lr 5e-3 -rtg --exp_name q2_b_$b
+done
+
+for lr in 0.005 0.01 0.02 0.03 0.04 0.05 do
+    python run_hw2.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.9 -n 100 -l 2 -s 64 -b 1000 -lr $lr -rtg --exp_name q2_lr_$lr
+done
+
+# q3
+python run_hw2.py --env_name LunarLanderContinuous-v2 --ep_len 1000 --discount 0.99 -n 100 -l 2 -s 64 -b 40000 -lr 0.005 --reward_to_go --nn_baseline --exp_name q3_b40000_r0.005
+
+# q4
+for b in 10000 30000 50000; do 
+    for r in 0.005 0.01 0.02; do
+        python run_hw2.py --env_name HalfCheetah-v2 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $b -lr $r -rtg --nn_baseline --exp_name q4_search_b$b"_lr"$r"_rtg_nnbaseline"
+    done
+done
+
+# TODO: find optimal b r
+
+# q5
+for gae_lambda in 0 0.95 0.99 1; do
+    python run_hw2.py --env_name Hopper-v2 --ep_len 1000 --discount 0.99 -n 300 -l 2 -s 32 -b 2000 -lr 0.001 --reward_to_go --nn_baseline --action_noise_std 0.5 --gae_lambda $gae_lambda --exp_name q5_b2000_r0.001_lambda$gae_lambda
+done
