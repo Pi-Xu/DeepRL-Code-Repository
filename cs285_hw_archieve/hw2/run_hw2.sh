9fc4520f32157617259185b20f9b1baa365cce26
wenqian <wenqian_xu@hotmail.com>
2022-10-14 10:37:12 +0800

    bug fix hw2 *.sh

--- cs285_hw_archieve/hw2/run_hw2.sh
+++ cs285_hw_archieve/hw2/run_hw2.sh
@@ -9,7 +9,7 @@ python run_hw2.py --env_name CartPole-v0 -n 100 -b 5000 -rtg --exp_name q1_lb_rt
 # q2
 for b in 1000 1500 2000 2500 3000; do 
     for lr in 0.005 0.01 0.02; do
-    python run_hw2.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.9 -n 100 -l 2 -s 64 -b $b -lr $lr -rtg --exp_name q2_b_$b_r_$lr
+    python run_hw2.py --env_name InvertedPendulum-v2 --ep_len 1000 --discount 0.9 -n 100 -l 2 -s 64 -b $b -lr $lr -rtg --exp_name q2_b_${b}_r_${lr}
     done
 done
 
@@ -19,7 +19,7 @@ python run_hw2.py --env_name LunarLanderContinuous-v2 --ep_len 1000 --discount 0
 # q4
 for b in 10000 30000 50000; do 
     for lr in 0.005 0.01 0.02; do
-        python run_hw2.py --env_name HalfCheetah-v2 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $b -lr $lr -rtg --nn_baseline --exp_name q4_search_b$b_lr$lr_rtg_nnbaseline
+        python run_hw2.py --env_name HalfCheetah-v2 --ep_len 150 --discount 0.95 -n 100 -l 2 -s 32 -b $b -lr $lr -rtg --nn_baseline --exp_name q4_search_b${b}_lr${lr}_rtg_nnbaseline
     done
 done
 
@@ -27,5 +27,5 @@ done
 
 # q5
 for gae_lambda in 0 0.95 0.99 1; do
-    python run_hw2.py --env_name Hopper-v2 --ep_len 1000 --discount 0.99 -n 300 -l 2 -s 32 -b 2000 -lr 0.001 --reward_to_go --nn_baseline --action_noise_std 0.5 --gae_lambda $gae_lambda --exp_name q5_b2000_r0.001_lambda$gae_lambda
+    python run_hw2.py --env_name Hopper-v2 --ep_len 1000 --discount 0.99 -n 300 -l 2 -s 32 -b 2000 -lr 0.001 --reward_to_go --nn_baseline --action_noise_std 0.5 --gae_lambda $gae_lambda --exp_name q5_b2000_r0.001_lambda${gae_lambda}
 done
