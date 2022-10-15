de1e3fedd70adb35b715d1bd8f156fc598ad73ae
wenqian <wenqian_xu@hotmail.com>
2022-10-15 11:11:18 +0800

    hw3 dqn bug fix

--- cs285_hw_archieve/hw3/cs285/agents/dqn_agent.py
+++ cs285_hw_archieve/hw3/cs285/agents/dqn_agent.py
@@ -69,6 +69,7 @@ class DQNAgent(object):
         # HINT2: remember the following useful function that you've seen before:
             #obs, reward, done, info = env.step(action)
         obs, reward, done, info = self.env.step(action)
+        self.last_obs = obs
 
         # TODO store the result of taking this action into the replay buffer
         # HINT1: see your replay buffer's `store_effect` function
